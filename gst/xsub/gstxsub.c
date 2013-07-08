73aa9545d3b3d5f44456734ba252934273dc342f
Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>
2013-07-08 13:59:45 -0400

    xsub: Port to 1.0. Drop _set_details_simple() call
    Use gst_element_class_set_metadata() instead

--- gst/xsub/gstxsub.c
+++ gst/xsub/gstxsub.c
@@ -128,7 +128,7 @@ gst_xsub_base_init (gpointer gclass)
 {
   GstElementClass *element_class = GST_ELEMENT_CLASS (gclass);
 
-  gst_element_class_set_details_simple (element_class,
+  gst_element_class_set_metadata (element_class,
       "XSub",
       "FIXME:Generic",
       "Decodes and Overlays XSUB Subtitles",
