6f9af197275ed6cb78980d5ab892f3068390a51e
Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>
2013-07-12 22:09:53 -0400

    new element: xsub decoder and mixer

---
+++ gst/xsub/gstxsub.c
@@ -0,0 +1,447 @@
+/*
+ * GStreamer plugin for the decoding and overlay of XSUB subtitles.
+ * Copyright (C) 2010 Collabora Multimedia
+ * Copyright (C) 2011-2013 Collabora Ltd.
+ *
+ * @Author: Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:element-xsub
+ *
+ * GStreamer plugin for the decoding and bliting of XSUB encoded
+ * SPU frames.
+ *
+ * <refsect2>
+ * <title>Example launch line</title>
+ * |[
+ * gst-launch-1.0 -v xsub name=overlay show-background=FALSE ! videoconvert
+ * ! videorate ! eglglessink filesrc location=/path/to/file.divx
+ * ! avidemux name=d d.video_0 ! queue ! mpeg4videoparse ! avdec_mpeg4
+ * ! videoconvert !  overlay.video_sink d.video_1 ! queue
+ * ! overlay.xsub_sink
+ * ]|
+ * </refsect2>
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <glib.h>
+
+#include "xsub.h"
+#include "gstxsub.h"
+
+GST_DEBUG_CATEGORY (xsub_debug);
+#define DEFAULT_SHOWBG FALSE
+
+enum
+{
+  PROP_0,
+  PROP_SHOW_BACKGROUND
+};
+
+/* The capabilities of the inputs and outputs.
+ */
+static GstStaticPadTemplate sink_factory_xsub =
+GST_STATIC_PAD_TEMPLATE ("xsub_sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("subpicture/x-xsub")
+    );
+
+static GstStaticPadTemplate sink_factory_pict =
+GST_STATIC_PAD_TEMPLATE ("video_sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (GST_VIDEO_CAPS_MAKE ("RGB"))
+    );
+
+static GstStaticPadTemplate src_factory_mixed = GST_STATIC_PAD_TEMPLATE ("src",
+    GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS (GST_VIDEO_CAPS_MAKE ("RGB"))
+    );
+
+G_DEFINE_TYPE (GstXSub, gst_xsub, GST_TYPE_ELEMENT);
+
+static void gst_xsub_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_xsub_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+
+static gboolean gst_xsub_set_video_caps (GstXSub * filter, GstCaps * caps);
+static GstFlowReturn gst_xsub_frame_chain (GstPad * pad, GstObject * parent,
+    GstBuffer * buf);
+static GstFlowReturn gst_xsub_spu_chain (GstPad * pad, GstObject * parent,
+    GstBuffer * buf);
+static gboolean xsub_sink_event_spu (GstPad * pad, GstObject * parent,
+    GstEvent * event);
+static gboolean xsub_sink_event_pic (GstPad * pad, GstObject * parent,
+    GstEvent * event);
+
+/* GObject vmethod implementations */
+
+/* Initialize the xsub's class */
+static void
+gst_xsub_class_init (GstXSubClass * klass)
+{
+  GObjectClass *gobject_class = (GObjectClass *) klass;
+  GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
+
+  gst_element_class_set_metadata (element_class,
+      "XSub",
+      "Mixer/Video/Overlay/Subtitle",
+      "Decodes and Overlays XSUB Subtitles",
+      "Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>");
+
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&src_factory_mixed));
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&sink_factory_xsub));
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&sink_factory_pict));
+
+  gobject_class->set_property = gst_xsub_set_property;
+  gobject_class->get_property = gst_xsub_get_property;
+
+  g_object_class_install_property (gobject_class, PROP_SHOW_BACKGROUND,
+      g_param_spec_boolean ("show-background", "Show Background",
+          "Show SPU Background ?", DEFAULT_SHOWBG,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+}
+
+/* Initialize the new element
+ * instantiate pads and add them to element
+ * set pad calback functions
+ * initialize instance structure
+ */
+static void
+gst_xsub_init (GstXSub * filter)
+{
+  g_mutex_init (&filter->spu_queue_lock);
+  g_queue_init (&filter->spu_queue);
+  filter->show_bg = DEFAULT_SHOWBG;
+
+  /* Picture in */
+  filter->video_sink = gst_pad_new_from_static_template (&sink_factory_pict,
+      "video_sink");
+  GST_PAD_SET_PROXY_CAPS (filter->video_sink);
+  GST_PAD_SET_PROXY_SCHEDULING (filter->video_sink);
+  GST_PAD_SET_PROXY_ALLOCATION (filter->video_sink);
+
+  gst_pad_set_chain_function (filter->video_sink,
+      GST_DEBUG_FUNCPTR (gst_xsub_frame_chain));
+  gst_pad_set_event_function (filter->video_sink,
+      GST_DEBUG_FUNCPTR (xsub_sink_event_pic));
+
+  /* Subpicture in */
+  filter->xsub_sink = gst_pad_new_from_static_template (&sink_factory_xsub,
+      "xsub_sink");
+  gst_pad_set_chain_function (filter->xsub_sink,
+      GST_DEBUG_FUNCPTR (gst_xsub_spu_chain));
+  gst_pad_set_event_function (filter->xsub_sink,
+      GST_DEBUG_FUNCPTR (xsub_sink_event_spu));
+
+  /* Mixed out */
+  filter->src = gst_pad_new_from_static_template (&src_factory_mixed, "src");
+
+  gst_element_add_pad (GST_ELEMENT (filter), filter->xsub_sink);
+  gst_element_add_pad (GST_ELEMENT (filter), filter->video_sink);
+  gst_element_add_pad (GST_ELEMENT (filter), filter->src);
+}
+
+static gboolean
+xsub_sink_event_spu (GstPad * pad, GstObject * parent, GstEvent * event)
+{
+  return gst_pad_event_default (pad, parent, event);
+}
+
+/* Handle events on picture sink pad */
+static gboolean
+xsub_sink_event_pic (GstPad * pad, GstObject * parent, GstEvent * event)
+{
+  GstCaps *caps;
+
+  switch (GST_EVENT_TYPE (event)) {
+    case GST_EVENT_CAPS:
+      gst_event_parse_caps (event, &caps);
+      if (!gst_xsub_set_video_caps ((GstXSub *) parent, caps)) {
+        gst_event_unref (event);
+        return FALSE;
+      }
+      break;
+    default:
+      break;
+  }
+
+  return gst_pad_event_default (pad, parent, event);
+}
+
+static void
+gst_xsub_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstXSub *filter = GST_XSUB (object);
+
+  switch (prop_id) {
+    case PROP_SHOW_BACKGROUND:
+      g_atomic_int_set (&filter->show_bg, g_value_get_boolean (value));
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_xsub_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstXSub *filter = GST_XSUB (object);
+
+  switch (prop_id) {
+    case PROP_SHOW_BACKGROUND:
+      g_value_set_boolean (value, g_atomic_int_get (&filter->show_bg));
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+/* GstElement vmethod implementations */
+
+static gboolean
+gst_xsub_set_video_caps (GstXSub * filter, GstCaps * caps)
+{
+  GstStructure *structure = gst_caps_get_structure (caps, 0);
+
+  gst_object_ref (filter);
+  gst_structure_get_int (structure, "width", &filter->width);
+  gst_structure_get_int (structure, "height", &filter->height);
+  gst_object_unref (filter);
+
+  return TRUE;
+}
+
+/* Chain function for frame data
+ *
+ * This function blends frame data with subpicture data when needed,
+ * otherwise it just copies frame data to output pad.
+ */
+static GstFlowReturn
+gst_xsub_frame_chain (GstPad * pad, GstObject * parent, GstBuffer * buf)
+{
+  GstXSub *filter;
+  GstXSubData *spu;
+  GstMapInfo pict_map, spu_map;
+  guint64 spu_start, spu_stop, frame_start, frame_stop;
+  int i;
+
+  filter = GST_XSUB (parent);
+
+  GST_DEBUG_OBJECT (pad, "Frame chain in width:%d height:%d", filter->width,
+      filter->height);
+
+  frame_start = GST_BUFFER_TIMESTAMP (buf);
+  frame_stop = frame_start + GST_BUFFER_DURATION (buf);
+
+  /* Here comes the blending logic */
+
+  g_mutex_lock (&filter->spu_queue_lock);
+
+  while ((spu = g_queue_peek_head (&filter->spu_queue))) {
+
+    spu_start = GST_BUFFER_TIMESTAMP (spu->buf);
+    spu_stop = spu_start + GST_BUFFER_DURATION (spu->buf);
+
+    if (frame_start < spu_start && frame_stop > spu_stop)
+      break;
+
+    if (frame_start >= spu_start) {
+
+      if (frame_start <= spu_stop)
+        break;
+
+      gst_buffer_unref (spu->buf);
+      g_slice_free1 (spu->width * 3, spu->bgless_row);
+      g_queue_remove (&filter->spu_queue, spu);
+      g_slice_free1 (sizeof (GstXSubData), spu);
+      continue;
+    }
+
+    /* We assume SPU packets to be ordered as they come */
+    spu = NULL;
+    break;
+  }
+
+  if (spu) {
+    GST_DEBUG_OBJECT (pad, "Have SPU data for this frame");
+    buf = gst_buffer_make_writable (buf);
+    if (!gst_buffer_map (buf, &pict_map, GST_MAP_READWRITE)) {
+      GST_ERROR_OBJECT (pad, "Map failed on picture buffer");
+      goto PUSH;
+    }
+
+    if (!gst_buffer_map (spu->buf, &spu_map, GST_MAP_READ)) {
+      GST_ERROR_OBJECT (pad, "Map failed on spu buffer");
+      gst_buffer_unmap (buf, &pict_map);
+      goto PUSH;
+    }
+
+    GST_DEBUG_OBJECT (pad, "pict/spu map succeed");
+
+    /* Overwrite coords if off bounds */
+
+    if (spu->coords[0] + spu->width > filter->width) {
+      GST_WARNING_OBJECT (pad, "X coord %hi off bounds, using 0 instead",
+          spu->coords[0]);
+      spu->coords[0] = 0;
+      spu->coords[2] = spu->width - 1;
+    }
+
+    if (spu->coords[1] + spu->height > filter->height) {
+      GST_WARNING_OBJECT (pad, "Y coord %hi off bounds, using 0 instead",
+          spu->coords[1]);
+      spu->coords[1] = 0;
+      spu->coords[3] = spu->height - 1;
+    }
+
+    /* FIXME: This should use the new overlay API */
+    if (filter->show_bg)
+      /* blit with background in */
+      for (i = 0; i < spu->height; i++)
+        memcpy (pict_map.data + (i * filter->width +
+                spu->coords[0]) * XSUB_RGB_BPP,
+            spu_map.data + i * spu->width * XSUB_RGB_BPP,
+            spu->width * XSUB_RGB_BPP);
+    else
+      /* blit without background */
+      for (i = 0; i < spu->height; i++) {
+        xsub_wipe_background (spu->bgless_row,
+            spu_map.data + i * spu->width * XSUB_RGB_BPP,
+            pict_map.data + (i * filter->width + spu->coords[0]) * XSUB_RGB_BPP,
+            spu->bg_color, spu->width * XSUB_RGB_BPP);
+
+        memcpy (pict_map.data + (i * filter->width +
+                spu->coords[0]) * XSUB_RGB_BPP, spu->bgless_row,
+            spu->width * XSUB_RGB_BPP);
+      }
+
+    GST_DEBUG_OBJECT (pad, "Blitted a spu!");
+    gst_buffer_unmap (buf, &pict_map);
+    gst_buffer_unmap (spu->buf, &spu_map);
+  }
+
+PUSH:
+  g_mutex_unlock (&filter->spu_queue_lock);
+  return gst_pad_push (filter->src, buf);
+}
+
+
+/* Chain function for subpicture data
+ *
+ * This function asks for the decoding of XSUB packets
+ * as they come and fills the SPU buffer
+ */
+static GstFlowReturn
+gst_xsub_spu_chain (GstPad * pad, GstObject * parent, GstBuffer * buf)
+{
+  GstXSub *filter;
+  GstXSubData *parsed;
+
+  filter = GST_XSUB (parent);
+
+  GST_DEBUG_OBJECT (pad, "SPU in");
+
+  parsed = g_slice_new (GstXSubData);
+  parsed->buf = gst_buffer_new ();
+
+  /* SPU decoding begins here */
+  if (!xsub_parse_header (pad, parsed, buf) ||
+      !xsub_parse_spu (pad, parsed, buf)) {
+
+    GST_ERROR_OBJECT (pad, "Parsing of header/spu data failed!");
+    gst_buffer_unref (parsed->buf);
+    g_slice_free (GstXSubData, parsed);
+  } else {
+    /* Save parsed data into spu_queue */
+    parsed->bgless_row = g_slice_alloc (parsed->width * XSUB_RGB_BPP);
+    g_mutex_lock (&filter->spu_queue_lock);
+    g_queue_push_tail (&filter->spu_queue, parsed);
+
+    GST_DEBUG_OBJECT (pad, "Parsed #%d: [(%hu,%hu)-(%hu,%hu)] Size: %zu",
+        g_queue_get_length (&filter->spu_queue),
+        parsed->coords[0], parsed->coords[1], parsed->coords[2],
+        parsed->coords[3], gst_buffer_get_size (parsed->buf));
+    g_mutex_unlock (&filter->spu_queue_lock);
+  }
+
+  return GST_FLOW_OK;
+}
+
+/* Entry point to initialize the plug-in
+ *
+ * Initialize the plug-in itself & register element factories and
+ * other features
+ */
+static gboolean
+xsub_init (GstPlugin * xsub)
+{
+  /* debug category for filtering log messages */
+
+  GST_DEBUG_CATEGORY_INIT (xsub_debug, "xsub", 0, "xsub");
+
+  return gst_element_register (xsub, "xsub", GST_RANK_NONE, GST_TYPE_XSUB);
+}
+
+/* GStreamer looks for this structure to register xsubs
+ */
+GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    xsub,
+    "GStreamer plugin for the decoding and overlay of XSUB subtitles",
+    xsub_init, VERSION, "LGPL", GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
+// vim: set expandtab tabstop=2 shiftwidth=2 autoindent smartindent:
---
+++ gst/xsub/xsub.c
@@ -0,0 +1,298 @@
+/*
+ * GStreamer plugin for the decoding and overlay of XSUB subtitles.
+ * Copyright (C) 2010 Collabora Multimedia
+ * Copyright (C) 2011-2013 Collabora Ltd.
+ *
+ * @Author: Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * Alternatively, the contents of this file may be used under the
+ * GNU Lesser General Public License Version 2.1 (the "LGPL"), in
+ * which case the following provisions apply instead of the ones
+ * mentioned above:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <gst/gst.h>
+#include <gst/base/gstbitreader.h>
+#include <gst/base/gstbytereader.h>
+#include <gst/base/gstbytewriter.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define XSUB_ASCII2_TOI(x,y) (((x) - '0') * 10 + ((y) - '0'))
+#define XSUB_ASCII3_TOI(x,y,z) (((x) - '0') * 100 + ((y) - '0') * 10 + ((z) - '0'))
+
+#include "xsub.h"
+
+static void xsub_parse_timing_string (GstBuffer * dest, const guint8 * t_frac);
+
+/* Wipe background color from a spu frame row replacing it with RGB24 frame
+ * color data.
+ */
+gboolean
+xsub_wipe_background (guint8 * dest, const guint8 * spu, const guint8 * src,
+    const guint32 * bg_color, guint16 stride)
+{
+  int i;
+
+  for (i = 0; i < stride; i += XSUB_RGB_BPP)
+    if ((GST_READ_UINT24_BE (spu + i)) == *bg_color >> 8)
+      memcpy (dest + i, src + i, XSUB_RGB_BPP);
+    else
+      memcpy (dest + i, spu + i, XSUB_RGB_BPP);
+
+  return TRUE;
+}
+
+/* Parses timing data from the first 27 bytes of an XSUB header
+ * Data comes as a non NULL terminated ASCII string with the following format:
+ * [HH:MM:SS.mmm-HH:MM:SS.mmm]
+ */
+static void
+xsub_parse_timing_string (GstBuffer * dest, const guint8 * t_frac)
+{
+  GST_BUFFER_TIMESTAMP (dest) = (GstClockTime)
+      ((XSUB_ASCII2_TOI (t_frac[1], t_frac[2]) * 3600
+          + XSUB_ASCII2_TOI (t_frac[4], t_frac[5]) * 60
+          + XSUB_ASCII2_TOI (t_frac[7], t_frac[8])) * GST_USECOND
+      + XSUB_ASCII3_TOI (t_frac[10], t_frac[11], t_frac[12])) * GST_MSECOND;
+  GST_BUFFER_DURATION (dest) = (GstClockTime)
+      ((XSUB_ASCII2_TOI (t_frac[14], t_frac[15]) * 3600
+          + XSUB_ASCII2_TOI (t_frac[17], t_frac[18]) * 60
+          + XSUB_ASCII2_TOI (t_frac[20], t_frac[21])) * GST_USECOND
+      + XSUB_ASCII3_TOI (t_frac[23], t_frac[24], t_frac[25])) * GST_MSECOND
+      - GST_BUFFER_TIMESTAMP (dest);
+}
+
+/* Parses the XSUB header from a XSUB packet */
+gboolean
+xsub_parse_header (GstPad * pad, GstXSubData * dest, GstBuffer * src)
+{
+  GstByteReader *header;
+  GstMapInfo map_info = GST_MAP_INFO_INIT;
+  gboolean ret = TRUE;
+  gboolean has_time = FALSE;
+  const guint8 *t_frac;
+
+  if (!gst_buffer_map (src, &map_info, GST_MAP_READ)) {
+    GST_ERROR_OBJECT (pad, "Error mapping src buffer");
+    return FALSE;
+  }
+
+  /* Quick sanity check */
+  if (map_info.size < XSUB_RLE_IMG_START) {
+    GST_ERROR_OBJECT (pad, "src buffer size < XSUB_RLE_IMG_START");
+    goto bail_mapped;
+  }
+
+  /* FIXME: fix demuxer to correctly parse xsub timing data and do:
+   * dest->buf->timestamp = src->timestamp;
+   * dest->buf->duration = src->duration;
+   * instead of all this time-parsing mambojambo
+   */
+
+  header = gst_byte_reader_new (map_info.data, XSUB_RLE_IMG_START);
+
+  /* Parse timing data if present */
+  if (map_info.data[0] == '[' && map_info.data[26] == ']') {
+
+    has_time = TRUE;
+
+    if (!gst_byte_reader_get_data (header, XSUB_SIZE_DATA_START, &t_frac)) {
+      GST_DEBUG_OBJECT (pad, "Error geting timing data");
+      goto bail_mapped;
+    } else {
+      xsub_parse_timing_string (dest->buf, t_frac);
+      GST_DEBUG_OBJECT (pad, "SPU TIMING: ts:%" GST_TIME_FORMAT " duration:%"
+          GST_TIME_FORMAT, GST_TIME_ARGS (GST_BUFFER_TIMESTAMP (dest->buf)),
+          GST_TIME_ARGS (GST_BUFFER_DURATION (dest->buf)));
+    }
+  }
+
+  if (!gst_byte_reader_get_int16_le (header, &dest->width) ||
+      !gst_byte_reader_get_int16_le (header, &dest->height) ||
+      !gst_byte_reader_get_int16_le (header, &dest->coords[0]) ||
+      !gst_byte_reader_get_int16_le (header, &dest->coords[1]) ||
+      !gst_byte_reader_get_int16_le (header, &dest->coords[2]) ||
+      !gst_byte_reader_get_int16_le (header, &dest->coords[3]) ||
+      !gst_byte_reader_get_int16_le (header, &dest->rle_length) ||
+      !gst_byte_reader_get_uint24_be (header, &dest->color_tab[0]) ||
+      !gst_byte_reader_get_uint24_be (header, &dest->color_tab[1]) ||
+      !gst_byte_reader_get_uint24_be (header, &dest->color_tab[2]) ||
+      !gst_byte_reader_get_uint24_be (header, &dest->color_tab[3])) {
+    GST_ERROR_OBJECT (pad, "Error byte-reading header data");
+    ret = FALSE;
+  }
+
+  GST_DEBUG_OBJECT (pad, "Parsed SPU w: %" G_GINT16_FORMAT
+      " h:%" G_GINT16_FORMAT " x0: %" G_GINT16_FORMAT
+      " y0:%" G_GINT16_FORMAT " x1: %" G_GINT16_FORMAT
+      " y1:%" G_GINT16_FORMAT, dest->width, dest->height,
+      dest->coords[0], dest->coords[1], dest->coords[2], dest->coords[3]);
+
+  dest->header_size = gst_byte_reader_get_pos (header);
+
+  if (!has_time)
+    dest->header_size -= XSUB_SIZE_DATA_START;
+
+  gst_byte_reader_free (header);
+
+  gst_buffer_unmap (src, &map_info);
+  return ret;
+
+bail_mapped:
+  gst_buffer_unmap (src, &map_info);
+  return FALSE;
+}
+
+/* Decodes RLE'd SPU data from a XSUB SPU frame whos header has
+ * already been parsed and puts RGB24 data for it in dest->buf
+ */
+gboolean
+xsub_parse_spu (GstPad * pad, GstXSubData * dest, GstBuffer * src)
+{
+  gint16 row, col;
+  guint16 needle;
+  guint16 run_length;
+  gint32 spu_size;
+  GstBitReader *haystack;
+  GstByteWriter *spu_frame;
+  GstMemory *spu_mem;
+  GstMapInfo src_map_info = GST_MAP_INFO_INIT;
+  GstMapInfo spu_map_info = GST_MAP_INFO_INIT;
+
+  int color_index, i, j;
+
+  if (!gst_buffer_map (src, &src_map_info, GST_MAP_READ)) {
+    GST_ERROR_OBJECT (pad, "Error mapping src buffer");
+    return FALSE;
+  }
+
+  /* Note that a full RLEd row of 1 color takes a ~byte */
+  if (!dest->width || !dest->height || dest->rle_length < dest->height) {
+    GST_ERROR_OBJECT (pad, "Wrong size or rle_length missmatch rle_length:%d",
+        dest->rle_length);
+    goto bail_src_mapped;
+  }
+
+  /* FIXME: Extend this beyond RGB 24bpp output */
+  spu_size = dest->width * dest->height * XSUB_RGB_BPP;
+  spu_mem = gst_allocator_alloc (NULL, spu_size, NULL);
+  gst_buffer_insert_memory (dest->buf, -1, spu_mem);
+
+  if (!gst_buffer_map (dest->buf, &spu_map_info, GST_MAP_WRITE)) {
+    GST_ERROR_OBJECT (pad, "Error mapping spu buffer");
+    gst_allocator_free (NULL, spu_mem);
+    gst_buffer_remove_all_memory (dest->buf);
+    goto bail_src_mapped;
+  }
+
+  /* SPU decoding */
+
+  /* Uncomment this and set the show-background
+   * property to TRUE to produce a black rectangle
+   * of the correct size for each spu. This is useful
+   * for testing correct positioning of the spu frame
+   * within the video frame.
+
+   memset (spu_map_info.data, 0, spu_size);
+   gst_buffer_unmap (src, &src_map_info);
+   gst_buffer_unmap (src, &spu_map_info);
+   return TRUE;
+
+   */
+
+  /* By the time we get here we know buffer has at least header_size bytes */
+  haystack = gst_bit_reader_new (src_map_info.data + dest->header_size,
+      dest->rle_length);
+  spu_frame = gst_byte_writer_new_with_data (spu_map_info.data,
+      gst_buffer_get_size (dest->buf), TRUE);
+
+  needle = 0;
+  for (row = 0; row < dest->height; row++) {
+
+    col = 0;
+    while (col < dest->width) {
+      run_length = 0;
+      for (i = 1; i < 5; i++) {
+        gst_bit_reader_get_bits_uint16
+            (haystack, &needle, i * XSUB_RLE_CHUNK_NBITS);
+
+        if (!(needle >> 2 * i)) {
+          if (i != 4)
+            gst_bit_reader_set_pos (haystack,
+                gst_bit_reader_get_pos (haystack) - XSUB_RLE_CHUNK_NBITS * i);
+          continue;
+        } else {
+          break;
+        }
+      }
+
+      run_length = (i == 5) ? dest->width - col : needle >> 2;
+      color_index = needle & 0x3;
+
+      /* Save background color
+       * We asume first pixel in will have the bg_color set */
+      if (!col && !row)
+        dest->bg_color = &(dest->color_tab)[color_index];
+
+      for (j = 0; j < run_length; j++)
+        gst_byte_writer_put_uint24_be (spu_frame,
+            (dest->color_tab)[color_index]);
+
+      col += run_length;
+    }
+
+    gst_bit_reader_skip_to_byte (haystack);
+  }
+
+  gst_bit_reader_free (haystack);
+  gst_byte_writer_free (spu_frame);
+  gst_buffer_unmap (src, &src_map_info);
+  gst_buffer_unmap (src, &spu_map_info);
+
+  return TRUE;
+
+bail_src_mapped:
+  gst_buffer_unmap (src, &src_map_info);
+  return FALSE;
+}
+
+// vim: set expandtab tabstop=2 shiftwidth=2 autoindent smartindent:
